1682bdb401bfbfe3b0793b5aad623103e57f965a
MaikGon <michal.gontarczyk@wp.pl>
2020-11-29 14:12:49 +0100

    First models trained

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import numpy as np
 
 def load_dataset(dataset_dir_path: Path) -> Tuple[np.ndarray, np.ndarray]:
     x, y = [], []
-    for i, class_dir in enumerate(dataset_dir_path.iterdir()):
+    for i, class_dir in enumerate(sorted(dataset_dir_path.iterdir())):
         for file in class_dir.iterdir():
             img_file = cv2.imread(str(file), cv2.IMREAD_GRAYSCALE)
             x.append(img_file)
@@ -35,11 +35,7 @@ def convert_descriptor_to_histogram(descriptors, vocab_model, normalize=True) ->
     return histogram
 
 
-def apply_feature_transform(
-        data: np.ndarray,
-        feature_detector_descriptor,
-        vocab_model
-) -> np.ndarray:
+def apply_feature_transform(data: np.ndarray, feature_detector_descriptor, vocab_model) -> np.ndarray:
     data_transformed = []
     for image in data:
         keypoints, image_descriptors = feature_detector_descriptor.detectAndCompute(image, None)
@@ -57,31 +53,28 @@ def project():
     np.random.seed(42)
 
     # TODO: fill the following values
-    first_name = 'John'
-    last_name = 'Doe'
+    first_name = 'Michał'
+    last_name = 'Gontarczyk'
 
-    data_path = Path('./../../test_data/')  # You can change the path here
+    data_path = Path('drive/My Drive/train')  # You can change the path here
     data_path = os.getenv('DATA_PATH', data_path)  # Don't change that line
     x, y = load_dataset(data_path)
-    x = data_processing(x)
 
     # TODO: create a detector/descriptor here. Eg. cv2.AKAZE_create()
-    feature_detector_descriptor = None
+    feature_detector_descriptor = cv2.BRISK_create()
 
     # TODO: train a vocabulary model and save it using pickle.dump function
-    with Path('vocab_model.p').open('rb') as vocab_file:  # Don't change the path here
+    with Path('drive/My Drive/vocab_model.p').open('rb') as vocab_file:  # Don't change the path here
         vocab_model = pickle.load(vocab_file)
 
     x_transformed = apply_feature_transform(x, feature_detector_descriptor, vocab_model)
 
     # TODO: train a classifier and save it using pickle.dump function
-    with Path('clf.p').open('rb') as classifier_file:  # Don't change the path here
+    with Path('drive/My Drive/clf.p').open('rb') as classifier_file:  # Don't change the path here
         clf = pickle.load(classifier_file)
 
     score = clf.score(x_transformed, y)
     print(f'{first_name} {last_name} score: {score}')
-    with Path(f'{last_name}_{first_name}_score.json').open('w') as score_file:  # Don't change the path here
-        json.dump({'score': score}, score_file)
 
 
 if __name__ == '__main__':
