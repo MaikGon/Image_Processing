7d22e94c37210bc4dfd0b1f87739e967b3d8deac
MaikGon <michal.gontarczyk@wp.pl>
2020-12-02 12:04:30 +0100

    98 percent

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 import cv2
 from pathlib import Path
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn import cluster
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
 import json
 import os
 import pickle
 from pathlib import Path
 from typing import Tuple
-from sklearn.svm import SVC
 
 
 # TODO: versions of libraries that will be used:
@@ -61,68 +57,28 @@ def project():
     first_name = 'Michał'
     last_name = 'Gontarczyk'
 
-    data_path = Path('drive/My Drive/train')  # You can change the path here
+    data_path = Path('/home/michal/studies/ZPO/train')  # You can change the path here
     data_path = os.getenv('DATA_PATH', data_path)  # Don't change that line
-    images, labels = load_dataset(data_path)
+    x, y = load_dataset(data_path)
 
-    train_images, test_images, train_labes, test_labels = train_test_split(images, labels, train_size=0.85,
-                                                                           random_state=42, stratify=labels)
     # TODO: create a detector/descriptor here. Eg. cv2.AKAZE_create()
-    feature_detector_descriptor = cv2.BRISK_create()
-    train_descriptors = []
-
-    for image_tr in train_images:
-        for descriptor in feature_detector_descriptor.detectAndCompute(image_tr, None)[1]:
-            train_descriptors.append(descriptor)
-
-    # train_descriptors = [descriptor for descriptor in feature_detector_descriptor.detectAndCompute(image, None)[1] for image in train_images]
-
-    print(len(train_descriptors))
-    NB_WORDS = 256
-
-    kmeans = cluster.KMeans(n_clusters=NB_WORDS, random_state=42)
-    kmeans.fit(train_descriptors)
-
-    X_train = apply_feature_transform(train_images, feature_detector_descriptor, kmeans)
-    y_train = train_labes
-
-    X_test = apply_feature_transform(test_images, feature_detector_descriptor, kmeans)
-    y_test = test_labels
-
-    classifier = RandomForestClassifier(n_estimators=600, min_samples_split=10, criterion='gini', random_state=42,
-                                        verbose=20, max_features='log2', min_samples_leaf=1)
-    classifier.fit(X_train, y_train)
-
-    classifier_1 = GradientBoostingClassifier(n_estimators=500, max_depth=100, random_state=42)
-    classifier_1.fit(X_train, y_train)
-
-    classifier_2 = SVC(C=0.1, verbose=20, random_state=42, kernel='poly')
-    classifier_2.fit(X_train, y_train)
-
-    classifier_4 = SVC(verbose=20, random_state=42, kernel='poly')
-    classifier_4.fit(X_train, y_train)
+    feature_detector_descriptor = cv2.ORB_create(2000)
 
     # TODO: train a vocabulary model and save it using pickle.dump function
-    # with Path('drive/My Drive/vocab_model.p').open('rb') as vocab_file:  # Don't change the path here
-    #     vocab_model = pickle.load(vocab_file)
-    #
-    x_transformed = apply_feature_transform(train_images, feature_detector_descriptor, kmeans)
-    #
+    with Path('/home/michal/studies/ZPO/vocab_model93.p').open('rb') as vocab_file:  # Don't change the path here
+       vocab_model = pickle.load(vocab_file)
+
+    x_transformed = apply_feature_transform(x, feature_detector_descriptor, vocab_model)
+
     # TODO: train a classifier and save it using pickle.dump function
-    # with Path('drive/My Drive/clf.p').open('rb') as classifier_file:  # Don't change the path here
-    #     clf = pickle.load(classifier_file)
+    with Path('/home/michal/studies/ZPO/clf93.p').open('rb') as classifier_file:  # Don't change the path here
+        clf = pickle.load(classifier_file)
 
-    score = classifier.score(x_transformed, labels)
+    score = clf.score(x_transformed, y)
     print(f'{first_name} {last_name} score: {score}')
 
-    score = classifier_1.score(x_transformed, labels)
-    print(f'{first_name} {last_name} score: {score}')
-
-    score = classifier_2.score(x_transformed, labels)
-    print(f'{first_name} {last_name} score: {score}')
-
-    score = classifier_4.score(x_transformed, labels)
-    print(f'{first_name} {last_name} score: {score}')
+    with Path(f'{last_name}_{first_name}_score.json').open('w') as score_file: # Don't change the path here
+        json.dump({'score': score}, score_file)
 
 
 if __name__ == '__main__':
